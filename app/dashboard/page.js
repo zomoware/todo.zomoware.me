2944efd971bb3ce625f067914f763dfd0fc6dc05
Ilyas Husain <134383694+zomowareceo@users.noreply.github.com>
2023-06-18 07:00:35 +0300

    [ADD] Button Animation

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,9 @@
+'use client';
+
+
 import AppLayout from '../../components/applayout';
 
+import {motion} from 'framer-motion';
 import {PlusCircle} from 'lucide-react';
 
 
@@ -7,8 +11,9 @@ export default function Dashboard() {
   return (
     <AppLayout>
       <h1 className=''>home</h1>
-      <div className='layout h-full w-full p-0'></div>
-      <button className='click w-64'><PlusCircle className='fill-black/20 dark:fill-white/20'/></button>
+      <div className='layout h-full w-full p-0'>
+      </div>
+      <motion.button initial={{opacity:0, y:20}} animate={{opacity:1, y:0}} className='click w-64'><PlusCircle className='fill-black/20 dark:fill-white/20'/></motion.button>
     </AppLayout>
   )
 }

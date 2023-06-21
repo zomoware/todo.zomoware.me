acab0e641df7e7e369d6d31e491485129a60f55d
Ilyas Husain <134383694+zomowareceo@users.noreply.github.com>
2023-06-22 02:45:51 +0300

    [NEW] Settings Page

--- app/dashboard/settings/page.js
+++ app/dashboard/settings/page.js
@@ -3,12 +3,34 @@ import Link from 'next/link';
 
 import AppLayout from '../../../components/applayout';
 
+import {ExternalLink, BadgeCheck, BadgeX} from 'lucide-react';
+
 
 export default function Settings() {
   return (
     <AppLayout>
       <h1 className=''>settings</h1>
-      <div className='layout h-full w-full p-0'></div>
+      <div className='layout flex-col h-full w-full gap-6 p-0'>
+      <div className='layout flex-col'>
+          <h2 className='opacity-100'>you account</h2>
+
+          <Link href={'https://my.zomoware.cloud'} className='click w-60'>my.zomoware.cloud <ExternalLink/></Link>
+        </div>
+
+        <div className='layout flex-col'>
+          <h2 className='opacity-100'>notifications</h2>
+
+          <div className='layout p-0'>
+            <p className='click w-60'><BadgeX className='fill-black/20 dark:fill-white/20'/> Email Notifications</p>
+            <button className='click w-20'>Enable</button>
+          </div>
+
+          <div className='layout p-0'>
+            <p className='click w-60'><BadgeCheck className='fill-black/20 dark:fill-white/20'/> App Notifications</p>
+            <button className='click w-20'>Disable</button>
+          </div>
+        </div>
+      </div>
     </AppLayout>
   )
 }
--- components/applayout.js
+++ components/applayout.js
@@ -9,22 +9,22 @@ import {Fingerprint, LayoutDashboard, Star, Clock, Settings} from 'lucide-react'
 
 export default function AppLayout({children}) {
   return (
-    <main className='layout flex-col-reverse h-screen w-full gap-0 p-0'>
+    <main className='layout flex-col h-screen w-full gap-0 p-0'>
+      <div className='theme border-b-2 h-20 w-full'>
+        <Link href={'/dashboard/settings'} className='click'><Fingerprint/> Ilyas Husain</Link>
+      </div>
+
+      <div className='layout h-full w-full'>
+        <main className='layout flex-col h-full w-full md:w-6/12 gap-0 p-0'>
+          {children}
+        </main>
+      </div>
+
       <div className='theme border-t-2 h-20 w-full gap-6 z-20'>
         <Link href={'/dashboard'} className='click'><LayoutDashboard className='fill-black/20 dark:fill-white/20'/></Link>
         <Link href={'/dashboard/stared'} className='click'><Star className='fill-black/20 dark:fill-white/20'/></Link>
         <Link href={'/dashboard/clock'} className='click'><Clock className='fill-black/20 dark:fill-white/20'/></Link>
         <Link href={'/dashboard/settings'} className='click'><Settings className='fill-black/20 dark:fill-white/20'/></Link>
-      </div>
-
-      <div className='layout h-full w-full'>
-        <main className='layout flex-col h-full w-9/12 md:w-7/12 gap-0 p-0'>
-          {children}
-        </main>
-      </div>
-
-      <div className='theme border-b-2 h-20 w-full'>
-        <Link href={'/dashboard/settings'} className='click'><Fingerprint/> Ilyas Husain</Link>
       </div>
     </main>
   )

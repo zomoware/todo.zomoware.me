a8293ab8a92c9cfb8dcd11ce1463bd0502e6e73e
Ilyas Husain <134383694+zomowareceo@users.noreply.github.com>
2023-06-27 01:53:29 +0300

    [ADD] Page Content

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import Link from 'next/link';
+
+
 export default function Home() {
   return (
-    <main className="layout h-screen w-screen p-0">
+    <main className="layout flex-col justify-evenly h-screen w-screen p-0">
+      <div className='layout flex-col gap-0 p-0'>
+        <h1>zomoware</h1>
+        <h2 className='opacity-50'>task</h2>
+      </div>
+
+      <Link href={'/dashboard'} className='click'>Login</Link>
     </main>
   )
 }
